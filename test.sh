fe2d1ce47f145e48a3744b809965427fd2f5ca7c
Rishab Kanwal <rishabkanwal@gmail.com>
2016-12-09 19:00:34 -0700

    Performance optimizations and cleanup

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-python test_formatter.py ./data/test.csv ./data/stores.csv ./data/stores/test.vw
+python test_formatter.py ./data/test.csv ./data/stores.csv ./data/features.csv ./data/stores/test.vw
 python3 split-shuffle.py 1.0 ./data/stores/test.vw ./data/stores/test-shuffled.vw
 vw -i sales.model -t -p ./results/predictions.txt < ./data/stores/test-shuffled.vw # Run vw
 python results_formatter.py ./results/predictions.txt ./data/test.csv ./results/results.txt # Run vw
--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
-python train_formatter.py ./data/train.csv ./data/stores.csv ./data/stores/train.vw
+python train_formatter.py ./data/train.csv ./data/stores.csv ./data/features.csv ./data/stores/train.vw
 python3 split-shuffle.py 1.0 ./data/stores/train.vw ./data/stores/train-shuffled.vw
-vw -f sales.model --passes=10 --cache_file=sales.cache --kill_cache --loss_function=quantile < ./data/stores/train-shuffled.vw
+vw -f sales.model --passes=10 --cache_file=sales.cache --kill_cache --nn 100 < ./data/stores/train-shuffled.vw
