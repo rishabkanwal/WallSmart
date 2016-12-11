a9600d94e3f0e9abe5f0533b8cc43b05538408c9
Rishab Kanwal <rishabkanwal@gmail.com>
2016-12-11 13:15:30 -0700

    Updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # WallSmart
+
+## Suite
+To run everything run use: bash suite.sh -type
+
+## Train
+To train use: bash train.sh -type
+
+## Test
+To test use: bash test.sh -type
+Test restults will be in results/results.txt
+
+## Type
+type values from above can be
+type = b for baseline, s for train with stores, f for train with features, fs train with features and stores
+
+## Vowpal Wabbit Commands
+To modify vw commands go to train.sh
+
+## Score and submission
+To submit go [here](https://www.kaggle.com/c/walmart-recruiting-store-sales-forecasting/submissions/attach)
+
--- train.sh
+++ train.sh
@@ -20,6 +20,5 @@ then
 fi
 
 python train_formatter.py ./data/train.csv ./data/stores.csv ./data/features.csv $OUTPUT_LOCATION$TRAIN $FLAG # Train with baseline
-echo python train_formatter.py ./data/train.csv ./data/stores.csv ./data/features.csv $OUTPUT_LOCATION$TRAIN $FLAG # Train with baseline
 python3 split-shuffle.py 1.0 $OUTPUT_LOCATION$TRAIN $OUTPUT_LOCATION$TRAIN_SHUFFLED
-vw -f sales.model --passes=20 --cache_file=sales.cache --kill_cache --loss_function=quantile < $OUTPUT_LOCATION$TRAIN_SHUFFLED
+vw -f sales.model --passes=20 --cache_file=sales.cache --kill_cache --loss_function=quantile -l2 < $OUTPUT_LOCATION$TRAIN_SHUFFLED
